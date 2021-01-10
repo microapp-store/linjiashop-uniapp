5e99392dc3ce875a16024d661ebfa24f745bdab8
microapp <eniluzt@qq.com>
2021-01-10 23:47:12 +0800

    Issue 引入微信支付sdk（h5端）

---
+++ pages/order/payment/callback.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="wrap">
+		<view class="top">
+			<u-icon name="checkmark-circle-fill" color="#19be6b" size="120" v-if="isSuccess && result!='查询中'"></u-icon>
+			<u-icon name="error-circle-fill" color="#fa3534" size="120" v-if="!isSuccess && result!='查询中'"></u-icon>
+			<view>{{result}}</view>
+		</view>
+		<view class="content" v-if="result!=='查询中'">
+			<u-button type="primary" @click="finish">确定</u-button> 
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				orderSn: '',
+				isSuccess: undefined,
+				result: '查询中'
+			}
+		},
+		onLoad(option) {
+			this.orderSn = option.orderSn
+			this.init()
+		},
+		methods: {
+			init() {
+				this.$u.get('pay/queryResult/' + this.orderSn).then(res => {
+					console.log('res', res)
+					if (res == true) {
+						this.result = "支付成功"
+						this.isSuccess = true
+					} else {
+						this.isSuccess = false
+						this.result = "支付失败"
+					}
+				})
+			},
+			finish(){ 
+				this.$u.route({
+					url: '/pages/order/detail',
+					params: {
+						orderSn: orderSn
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.wrap {
+		.top {
+			text-align: center;
+			margin:60rpx;
+		}
+		.content{
+			padding:60rpx;
+		}
+	}
+</style>
--- pages/order/payment/payment.vue
+++ pages/order/payment/payment.vue
@@ -20,13 +20,14 @@
 				</u-row>
 			</view>
 		</u-radio-group>
-		
+
 		<u-button type="primary" @click="submit">立即支付</u-button>
 
 	</view>
 </template>
 
 <script>
+	import wx from '@/common/weixin-js-sdk'
 	export default {
 		data() {
 			return {
@@ -55,10 +56,102 @@
 			this.orderSn = option.orderSn;
 			this.totalPrice = option.totalPrice;
 		},
-		methods: { 
-			submit(){
-				console.log('payType',this.payType);
-				this.$u.toast(this.payType+'支付完善中'); 
+		methods: {
+			submit() {
+				console.log('payType', this.payType);
+				// #ifdef H5
+				if ('wxpay' === this.payType) {
+					this.$u.post('pay/wx/prepare?orderSn=' + this.orderSn).then(res => {
+						console.log('res',res)
+						this.wxPayH5({
+								appId: res.appId,
+								nonceStr: res.nonceStr,
+								package: res.packageValue,
+								paySign: res.paySign,
+								signType: res.signType,
+								timeStamp: res.timeStamp
+							}
+
+						)
+					})
+				}
+				// #endif
+				// this.$u.toast(this.payType+'支付完善中'); 
+				// #ifndef H5
+				if ('wxpay' === this.payType) {
+					this.$u.post('pay/wx/prepare?orderSn=' + this.orderSn).then(res => {
+						uni.requestPayment({
+							provider: 'wxpay',
+							orderInfo: res,
+							success: (e) => {
+								console.log("success", e);
+								uni.showToast({
+									title: "完成支付(功能测试种)!"
+								})
+							},
+							fail: (e) => {
+								console.log("fail", e);
+								uni.showModal({
+									content: "支付失败,原因为: " + e.errMsg,
+									showCancel: false
+								})
+							},
+							complete: () => {
+								// this.providerList[index].loading = false;
+							}
+						})
+					});
+				}
+				// #endif
+			},
+			wxPayH5: function(data) {
+				//获取后台传入的数据
+				let appId = data.appId
+				let timestamp = data.timeStamp
+				let nonceStr = data.nonceStr
+				let signature = data.signature
+				let packageValue = data.package
+				let paySign = data.paySign
+				let signType = data.signType
+				const that = this
+				//下面要发起微信支付了
+				wx.config({
+					debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+					appId: appId, // 必填，公众号的唯一标识
+					timestamp: timestamp, // 必填，生成签名的时间戳
+					nonceStr: nonceStr, // 必填，生成签名的随机串
+					signature: signature, // 必填，签名，见附录1
+					jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+				})
+				wx.ready(function() {
+					wx.chooseWXPay({
+						timestamp: timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+						nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
+						package: packageValue, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+						signType: signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+						paySign: paySign, // 支付签名
+						success: function(res) {
+							// 支付成功后的回调函数
+							that.$router.push('/payment/callback/' + that.order.orderSn)
+							this.$u.route({
+								url: '/pages/order/payment/callback',
+								params: {
+									orderSn: orderSn
+								}
+							})
+						},
+						fail: function(res) {
+							//失败回调函数
+							Toast('支付失败')
+						}
+					})
+				})
+				wx.error(function(res) {
+					console.log('微信支付失败', res)
+					Toast('支付失败')
+					// config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+					/*alert("config信息验证失败")*/
+				})
 			}
 		}
 	}
@@ -72,7 +165,7 @@
 		.item {
 			width: 100%;
 			padding: 20rpx 10rpx;
- 
+
 		}
 
 
