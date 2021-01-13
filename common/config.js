b6ad446e476a59feb8e50d07c35c058de7ca9f6f
microapp <eniluzt@qq.com>
2021-01-13 10:24:40 +0800

    Change config.js

--- common/config.js
+++ common/config.js
@@ -1,5 +1,5 @@
 // config.js，根目录的common文件夹中
 export default {
-	baseApi: 'http://wt1hd.microapp.store/prod-api',
-	//baseApi: 'http://localhost:8081/prod-api',
+	baseApi: 'https://wt1hd.microapp.store/prod-api',
+	// baseApi: 'http://localhost:8081',
 }
