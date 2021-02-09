be8b60c8a0c46c7aa57221c04bed1703cc2908d6
microapp <eniluzt@qq.com>
2021-02-09 22:44:17 +0800

    Change config.js

--- common/config.js
+++ common/config.js
@@ -1,6 +1,6 @@
 // config.js，根目录的common文件夹中
 export default {
 	// baseApi: 'https://linjiashop.microapp.store/prod-api'
-	baseApi:'prod-api'//web生产
-	// baseApi: 'http://localhost:8081',//本地测试
+	baseApi:'/prod-api'//web生产
+	// baseApi: 'http://localhost:8081'//本地测试
 }
